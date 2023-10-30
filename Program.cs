cb171cd4b66e2d6a858fe4b52c29cc9f140930a8
Mughees-Yasir <146824921+Mughees-Yasir@users.noreply.github.com>
2023-10-30 07:55:19 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Factor
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int i = 2, n;
+            Console.WriteLine("Enter a value");
+            n = int.Parse(Console.ReadLine());
+            while (i <= n)
+            {
+                if (n < 10 && i <= n)
+                {
+                    Console.WriteLine(n + "Dont have any single digit factor other then one");
+                    break;
+                }
+
+                if (n % i == 0)
+                {
+                    Console.WriteLine(i + "is smallest factor");
+                    break;
+                }
+
+                i++;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
